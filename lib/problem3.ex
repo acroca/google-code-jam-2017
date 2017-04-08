ae0522742b5c1b110d010831c1c282fb9b039be8
Albert Callarisa <albert@acroca.com>
2017-04-08 15:33:46 -0700

    #3 using heap and map for counts.

--- lib/problem3.ex
+++ lib/problem3.ex
@@ -14,47 +14,82 @@ defmodule Gcj.Problem3 do
   end
 
   def process({bathrooms, people}) do
-    s = Heap.max
-    |> Heap.push({bathrooms, round(Float.floor(bathrooms/2.0))})
+    last = {
+      Heap.max |> Heap.push(bathrooms), # For next larger gap
+      Map.new |> Map.put(bathrooms, 1) # For counts
+    }
     |> last_bathroom(people)
-    |> spots()
-    |> Enum.map(&elem(&1,0))
-    |> Enum.join(" ")
+    |> elem(0)
+    |> Heap.root()
+    "#{lspots(last)} #{rspots(last)}"
   end
 
-  def last_bathroom(heap, 1) do
-    Heap.root(heap)
+  def last_bathroom(data, 0) do
+    data
   end
 
-  def last_bathroom(heap, n) do
-    # TODO in case of overlap, find the highest index
-    heap = Heap.root(heap)
-    |> spots()
-    |> Enum.into(Heap.pop(heap))
+  def last_bathroom({heap, counts} = data, n) do
+    gap_size = Heap.root(heap)
+    amount = Map.get(counts, gap_size)
+    if amount < n do
+      heap = Heap.pop(heap)
+      counts = Map.delete(counts, gap_size)
+      ls = lspots(gap_size)
+      rs = rspots(gap_size)
+      if (Map.get(counts, ls, 0) == 0) do
+        heap = Heap.push(heap, ls)
+      end
+      if rs != ls && Map.get(counts, rs, 0) == 0 do
+        heap = Heap.push(heap, rs)
+      end
+      counts = Map.update(counts, ls, amount, &(&1 + amount))
+      counts = Map.update(counts, rs, amount, &(&1 + amount))
 
-    last_bathroom(heap, n-1)
+      last_bathroom({
+        heap,
+        counts
+      }, n - amount)
+    else
+      data
+    end
+  end
+
+  def reprocess_heap(heap) do
+    heap
+    |> Enum.reduce([], fn(v,acc) ->
+      acc ++ [lspots(v), rspots(v)]
+    end)
+    |> Enum.into(Heap.max)
   end
 
   @doc ~S"""
   ## Examples
-      iex> Gcj.Problem3.spots {10,5}
-      [{5,2},{4,8}]
-      iex> Gcj.Problem3.spots {4,8}
-      [{2,7},{1,9}]
-      iex> Gcj.Problem3.spots {5,8}
-      [{2,7},{2,10}]
-      iex> Gcj.Problem3.spots {1,3}
-      [{0,-1}, {0,-1}]
-      iex> Gcj.Problem3.spots {2,3}
-      [{1,2},{0,4}]
+      iex> Gcj.Problem3.lspots 10
+      5
+      iex> Gcj.Problem3.lspots 4
+      2
+      iex> Gcj.Problem3.lspots 5
+      2
+      iex> Gcj.Problem3.lspots 1
+      0
+      iex> Gcj.Problem3.lspots 2
+      1
   """
-  def spots({1,_}), do: [{0,-1}, {0,-1}]
-  def spots({size,index}) do
-    ls = round(Float.ceil((size-1)/2.0))
-    rs = round(Float.floor((size-1)/2.0))
-    li = index - round(Float.ceil(ls/2.0))
-    ri = index + 1 + round(Float.floor(rs/2.0))
-    [{ls, li}, {rs, ri}]
-    |> Enum.filter(fn({s,_}) -> s > 0 end)
-  end
+  def lspots(1), do: 0
+  def lspots(size), do: round(Float.ceil((size-1)/2.0))
+  @doc ~S"""
+  ## Examples
+      iex> Gcj.Problem3.rspots 10
+      4
+      iex> Gcj.Problem3.rspots 4
+      1
+      iex> Gcj.Problem3.rspots 5
+      2
+      iex> Gcj.Problem3.rspots 1
+      0
+      iex> Gcj.Problem3.rspots 2
+      0
+  """
+  def rspots(1), do: 0
+  def rspots(size), do: round(Float.floor((size-1)/2.0))
 end
